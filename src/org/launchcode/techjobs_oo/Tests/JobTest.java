5abfc058b13ff171d37a2c59bbb5c518b45f1e4c
AmandaHEC <93448955+AmandaHEC@users.noreply.github.com>
2022-01-31 14:22:02 -0600

    Through#4

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,44 @@
+//MINE
+package org.launchcode.techjobs_oo.Tests;
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+import static org.junit.Assert.*;
+
+public class JobTest {
+    private Job jobOne, jobTwo, jobThree;
+
+
+    @Before
+    public void testJobObjects(){
+        jobOne = new Job("Dog walker", new Employer("Purina" ), new Location("St. Louis"),
+                new PositionType("entry level"), new CoreCompetency("Java"));
+        jobTwo = new Job("Cat walker", new Employer("PetCo" ), new Location("St. Louis"),
+                new PositionType("entry level"), new CoreCompetency("Java"));
+        jobThree = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test public void testSettingJobId() {
+
+        assertEquals(1, jobOne.getId());
+        assertEquals(2, jobTwo.getId());
+        assertEquals(3, jobThree.getId());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertTrue(jobThree.getEmployer() instanceof Employer);
+        assertTrue(jobThree.getLocation() instanceof Location);
+        assertTrue(jobThree.getPositionType() instanceof PositionType);
+        assertTrue(jobThree.getCoreCompetency() instanceof CoreCompetency);
+    }
+@Test
+public void testJobsForEquality(){
+        assertFalse(jobOne.equals(jobTwo));
+        assertFalse(jobOne.equals(jobThree));
+
+}
+
+
+}
+//^^MINE
